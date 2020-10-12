fb0f6fb3768d45508437b235239cce6458404fa9
Maria Alvarado <if715373@iteso.mx>
2020-10-12 16:45:50 -0500

    HW:BORROWERS

--- src/homework/borrowers/main.py
+++ src/homework/borrowers/main.py
@@ -11,12 +11,11 @@ class Main:
     @staticmethod
     def show(file: str = DEFAULT_FILENAME) -> str:
         logger.info("Calling the show method.")
-        with open(file,'r') as f:
-           return(f.read())
-        # TODO: read file and show content
-
-
+        with open(file, 'r') as f:
+            return (f.read())
+    # TODO: read file and show content
     # TODO: read file an
+
     @staticmethod
     def insert(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
         borrower = Borrower(email=email, age=age, income=income)
