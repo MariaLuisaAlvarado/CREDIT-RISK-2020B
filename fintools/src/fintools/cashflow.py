e9d71475c20ce05d01a0792688ff3baa0116f1c4
Maria Alvarado <if715373@iteso.mx>
2020-10-27 23:28:08 -0600

    HW:CASHFLOW

--- fintools/src/fintools/cashflow.py
+++ fintools/src/fintools/cashflow.py
@@ -8,13 +8,37 @@ class CashFlow:
         self.n = n
 
     def pv(self, r: float) -> 'CashFlow':
-        raise NotImplementedError
+        r = r
+        res = self.amount/(1+r)**self.n
+        ans = CashFlow(res, 0)
+        return ans
+
+    def fv(self, r: float) -> 'CashFlow':
+        r = r
+        res = self.amount*(1+r)**self.n
+        ans = CashFlow(res, self.n)
+        return ans
 
     def shift(self, n: int, r: float) -> 'CashFlow':
-        raise NotImplementedError
+        if self.n > n:
+            return CashFlow(self.amount, n).fv(r)
+        else:
+            return CashFlow(self.amount, n).pv(r)
 
     def merge(self, other: 'CashFlow', r: float, reverse: bool = False) -> 'CashFlow':
-        raise NotImplementedError
+        if reverse is not True:
+            res1, res2 = self.pv(r), other.pv(r)
+            ans = res1 + res2
+            return ans
+        else:
+            if self.n > other.n:
+                res1 = self
+                res2 = other.shift(self.n, r)
+            else:
+                res1 = self.shift(other.n, r)
+                res2 = other
+            ans = res1 + res2
+            return ans
 
     def to_dict(self, decimal_places: Optional[int] = 2) -> Dict:
         return {
--- src/classwork/cashflow/main.py
+++ src/classwork/cashflow/main.py
@@ -1,5 +1,4 @@
 import json
-
 from fintools import CashFlow
 
 
@@ -7,8 +6,14 @@ class Main:
 
     @staticmethod
     def present_value(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        res = CashFlow(amount, n)
+        res2 = res.pv(rate)
+        ans = res2.to_dict()
+        return ans
 
     @staticmethod
     def future_value(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        res = CashFlow(amount, n)
+        res2 = res.fv(rate)
+        ans = res2.to_dict()
+        return ans
